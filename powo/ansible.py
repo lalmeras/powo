79be2355898c90649059fbc7698b18681cd7e090
Laurent Almeras <lalmeras@gmail.com>
2016-11-02 23:46:31 +0100

    wip - custom stdout_callback

--- powo/ansible.py
+++ powo/ansible.py
@@ -6,16 +6,17 @@ import os.path
 import pkg_resources
 import sys
 
+from ansible import constants as C
 from ansible.cli import CLI
 from ansible.cli.galaxy import GalaxyCLI
 from ansible.parsing.dataloader import DataLoader
 from ansible.vars import VariableManager
 from ansible.inventory import Inventory
 from ansible.playbook.play import Play
+from ansible.plugins.callback import CallbackBase
 from ansible.utils.unicode import to_bytes
 from ansible import utils
 from ansible.executor.task_queue_manager import TaskQueueManager
-from ansible import constants as C
 
 import click
 import yaml
@@ -152,7 +153,7 @@ def update(ctx, ask_become_pass, **kwargs):
                   loader=loader,
                   options=options,
                   passwords=passwords,
-                  stdout_callback='skippy'
+                  stdout_callback=CallbackModule()
         )
         tqm.run(play)
     finally:
@@ -164,3 +165,74 @@ plugins = load_plugins()
 for plugin in plugins:
     if plugin.decorate_update:
         plugin.decorate_update(update)
+
+
+import pprint
+
+
+class CallbackModule(CallbackBase):
+    def on_any(self, *args, **kwargs):
+        pass
+
+    def runner_on_failed(self, host, res, ignore_errors=False):
+        pprint.pprint(res)
+
+    def runner_on_ok(self, host, res):
+        pprint.pprint(res)
+
+    def runner_on_error(self, host, msg):
+        pprint.pprint(msg)
+
+    def runner_on_skipped(self, host, item=None):
+        pprint.pprint(item)
+
+    def runner_on_unreachable(self, host, res):
+        pass
+
+    def runner_on_no_hosts(self):
+        pass
+
+    def runner_on_async_poll(self, host, res, jid, clock):
+        pass
+
+    def runner_on_async_ok(self, host, res, jid):
+        pass
+
+    def runner_on_async_failed(self, host, res, jid):
+        pass
+
+    def playbook_on_start(self):
+        pass
+
+    def playbook_on_notify(self, host, handler):
+        pass
+
+    def playbook_on_no_hosts_matched(self):
+        pass
+
+    def playbook_on_no_hosts_remaining(self):
+        pass
+
+    def playbook_on_task_start(self, name, is_conditional):
+        print name
+        pass
+
+    def playbook_on_vars_prompt(self, varname, private=True, prompt=True,
+                                encrypt=None, confirm=False, salt_size=None,
+                                salt=None, default=None):
+        pass
+
+    def playbook_on_setup(self):
+        pass
+
+    def playbook_on_import_for_host(self, host, imported_file):
+        pass
+
+    def playbook_on_not_import_for_hosts(self, host, missing_file):
+        pass
+
+    def playbook_on_play_start(self, pattern):
+        pass
+
+    def playbook_on_stats(self, stats):
+        pass
