2a1c8b768665c4cd85b6fec0d6dfec61a74879a5
Laurent Almeras <laurent.almeras@openwide.fr>
2017-08-25 11:13:29 +0200

    Bump version: 1.0.0 → 1.0.1rc

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='powo',
-    version='1.0.0',
+    version='1.0.1rc',
     description="powo installer",
     long_description=readme + '\n\n' + history,
     author="Laurent Almeras",
